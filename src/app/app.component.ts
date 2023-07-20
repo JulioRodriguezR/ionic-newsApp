e6a43c83f6c0226a1d0d072497daf8f33c6cd560
jrodriguer <juliorodriguezramirez@gmail.com>
2023-07-21 01:09:16 +0200

    move to constructor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,8 +5,6 @@ import { Platform } from '@ionic/angular'
 import { PushService } from './services/push.service'
 import { register } from 'swiper/element/bundle';
 
-register();
-
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
@@ -19,6 +17,7 @@ export class AppComponent {
     private statusBar: StatusBar,
     private pushNotification: PushService
   ) {
+    register();
     this.initializeApp()
   }
 
